c858b39848d62675d52267b1c40157e80fb88f28
Bar Rodoy <112381346+barrodoy@users.noreply.github.com>
2023-09-01 21:38:43 +0300

    Delete src/secrets.h

--- src/secrets.h
+++
@@ -1,8 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "rodoy"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "0544543538"	// replace MyPassword with your WiFi password
-
-#define SECRET_CH_ID 2209504			// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "OF6CVEMHFWTZXMYO"   // replace XYZ with your channel write API Key
